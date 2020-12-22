038883e974d4d40d4b98083f7556acffb8f7c54e
Moritz <moritz.banhardt@web.de>
2020-12-22 13:00:01 +0100

    init push service in app.component

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+  Capacitor,
+} from '@capacitor/core';
+import { Router } from '@angular/router';
+
+const { PushNotifications } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  constructor() { }
+  constructor(private router: Router) {}
+  
+  public initPush() {
+    if (Capacitor.platform !== 'web') {
+      this.registerPush();
+    }
+  }
+
+  private registerPush() {
+      // Request permission to use push notifications
+      // iOS will prompt user and return if they granted permission or not
+      // Android will just grant without prompting
+      PushNotifications.requestPermission().then( result => {
+        if (result.granted) {
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+        }
+      });
+  
+      // On success, we should be able to receive notifications
+      PushNotifications.addListener('registration',
+        (token: PushNotificationToken) => {
+          alert('Push registration success, token: ' + token.value);
+        }
+      );
+  
+      // Some issue with our setup and push will not work
+      PushNotifications.addListener('registrationError',
+        (error: any) => {
+          alert('Error on registration: ' + JSON.stringify(error));
+        }
+      );
+  
+      // Show us the notification payload if the app is open on our device
+      PushNotifications.addListener('pushNotificationReceived',
+        (notification: PushNotification) => {
+          alert('Push received: ' + JSON.stringify(notification));
+        }
+      );
+  
+      // Method called when tapping on a notification
+      PushNotifications.addListener('pushNotificationActionPerformed',
+        (notification: PushNotificationActionPerformed) => {
+          alert('Push action performed: ' + JSON.stringify(notification));
+        }
+      );
+    }
 }
