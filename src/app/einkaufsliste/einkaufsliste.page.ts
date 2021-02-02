4c8a89f695d90e89d604e8148fc2299627eafff6
Fabienne <Fabi>
2021-02-02 10:17:00 +0100

    einkaufliste array,

--- src/app/einkaufsliste/einkaufsliste.page.ts
+++ src/app/einkaufsliste/einkaufsliste.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./einkaufsliste.page.scss'],
 })
 export class EinkaufslistePage implements OnInit {
-
+  Listitems = ['first', 'second'];
   constructor() { }
 
   ngOnInit() {
