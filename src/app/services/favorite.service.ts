0289700c42958a5cce0955048d0aa7fa1c3e13f2
Moritz <moritz.banhardt@web.de>
2021-02-09 16:22:54 +0100

    bug add fixed

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from 
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { Angebot } from './angebote.service';
+import { ToastController } from '@ionic/angular';
 
 export interface Favorite {
   id?: string,
@@ -19,7 +20,7 @@ export class FavoriteService {
   private favorites: Observable<Favorite[]>;
   private favoritesCollection: AngularFirestoreCollection<Favorite>;
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private afs: AngularFirestore, private toastController: ToastController) {
     this.favoritesCollection = this.afs.collection<Favorite>('favorites');
     this.favorites = this.favoritesCollection.snapshotChanges().pipe(
       map(actions => {
@@ -47,11 +48,30 @@ export class FavoriteService {
   }
 
   addFavorite(favorite: Favorite): Promise<DocumentReference> {
-    return this.favoritesCollection.add(favorite);
+    this.afs.collection(`favorites`, ref => ref.where('title', "==", favorite.title)).snapshotChanges().subscribe(res => {
+      if (res.length > 0)
+      {
+        this.presentToast("Favorit schon in der Liste");
+        return null;
+      }
+      else
+      {
+        return this.favoritesCollection.add(favorite);
+      }
+    });
+    return null;
   }
 
   deleteFavorite(id: string): Promise<void> {
     return this.favoritesCollection.doc(id).delete();
+  }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
